525f14622174e386ccd4b7b1a6872b288112433e
AmBplus <96792239+AmBplus@users.noreply.github.com>
2022-03-26 17:57:37 +0430

    app secound version Ui Ended ! Event For btn Created ...

--- WpfCustomerService/MainWindow.xaml.cs
+++ WpfCustomerService/MainWindow.xaml.cs
@@ -1,28 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace WpfCustomerService
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BtnHome_OnClick(object sender, RoutedEventArgs e)
+        {
+            HomePanel.Visibility = Visibility.Collapsed;
+        }
+
+        private void BtnEmploys_OnClick(object sender, RoutedEventArgs e)
+        {
+            EmploysPanel.Visibility = Visibility.Collapsed;
+        }
+
+        private void BtnCustomers_OnClick(object sender, RoutedEventArgs e)
+        {
+            customerPanel.Visibility = Visibility.Collapsed;
+        }
+
+        private void BtnProducts_OnClick(object sender, RoutedEventArgs e)
+        {
+            ProductsPanel.Visibility = Visibility.Collapsed;
         }
     }
 }
